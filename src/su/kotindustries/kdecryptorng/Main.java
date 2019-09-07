aa1f96817b269ab6a0005b3b45e68cba0e1aee78
AlexKot97 <alex.g97@yandex.ru>
2019-09-07 15:31:26 +0300

    Add TranslateMatrix fetch

--- src/su/kotindustries/kdecryptorng/Main.java
+++ src/su/kotindustries/kdecryptorng/Main.java
@@ -6,21 +6,18 @@ public class Main {
 
     public static void main(String[] args) {
         String[] asDictionary;
-        String sDictionary;
+        String sDictionaryFileName;
         String[] asMasks = new String[2];
-
-        List<String> lsRes1, lsRes2;
-
 
         IO io = new IO();
         MrBrute mb = new MrBrute();
 
-        sDictionary = "default.dic";
+        sDictionaryFileName = "default.dic";
         if (args.length > 1)
             if (io.dir(args[0]))
-                sDictionary = args[0];
-        io.println("Dictionary path: " + sDictionary);
-        asDictionary = io.loadDictionary(sDictionary);
+                sDictionaryFileName = args[0];
+        io.println("Dictionary path: " + sDictionaryFileName);
+        asDictionary = io.loadDictionary(sDictionaryFileName);
         mb.setDictionary(asDictionary);
 
         io.println("Enter a mask 1: ");
@@ -38,24 +35,40 @@ public class Main {
         }
         io.println(" Done");
 
-        lsRes1 = mb.getResult(0);
-        lsRes2 = mb.getResult(1);
+        String[][] lsRes1 = mb.getResult(0);
+        String[][] lsRes2 = mb.getResult(1);
 
         int iMatchCount1 = mb.getMatchCount(0);
         int iMatchCount2 = mb.getMatchCount(1);
+
+        String sSign1 = mb.getSignature(0);
+        String sSign2 = mb.getSignature(1);
+
         int iWordsCount = mb.getDictionarySize();
+
         io.println();
         io.println("            STATISTICS           ");
         io.println("Dictionary power, words: " + iWordsCount + ", matches found: " + iMatchCount1 + ", " + iMatchCount2);
         io.println();
         io.println("              MATCHES            ");
 
-        for (String sLine : lsRes1) {
-            io.print(sLine + " ");
+
+
+        io.println("Signature 1: " + sSign1);
+        for (String[] sLine : lsRes1) {
+            io.print(" [");
+            for (String sPara : sLine)
+                io.print(" "+sPara);
+            io.print("]");
         }
         io.println();
-        for (String sLine : lsRes2) {
-            io.print(sLine + " ");
+        io.println("Signature 2: " + sSign2);
+
+        for (String[] sLine : lsRes2) {
+            io.print(" [");
+            for (String sPara : sLine)
+                io.print(" "+sPara);
+            io.print("]");
         }
         io.println();
     }
--- src/su/kotindustries/kdecryptorng/MrBrute.java
+++ src/su/kotindustries/kdecryptorng/MrBrute.java
@@ -1,7 +1,6 @@
 package su.kotindustries.kdecryptorng;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 class MrBrute {
     private Mask[] amMasks;
@@ -27,34 +26,41 @@ class MrBrute {
         amMasks[1].checkMatch(asDictionary[iWord]);
     }
 
-    List<String> getResult(Integer iIndex) {
-        return amMasks[iIndex].getMatches();
+    String[][] getResult(Integer iIndex) {
+        return amMasks[iIndex].getMatrixArray();
     }
 
     int getMatchCount(Integer iIndex) {
-        return amMasks[iIndex].getMatchCount();
+        return amMasks[iIndex].getMatrixCount();
     }
 
     int getDictionarySize() {
         return iDictionarySize;
     }
 
+    String getSignature(int iIndex) {
+        return amMasks[iIndex].getSignature();
+    }
+
     static class Mask {
         private String sMask;
         private String sSignature;
-        private List<String> lsMatch;
+        //private List<String> lsMatch;
+        private List<String[]> lasMatrix;
 
         Mask(String sWord) {
             sMask = sWord;
             this.sSignature = calcSignature(sWord);
-            lsMatch = new ArrayList<>();
+            //lsMatch = new ArrayList<>();
+            lasMatrix = new ArrayList<>();
         }
 
         void checkMatch(String sWord) {
             String sWordSignature = calcSignature(sWord);
             if (sWordSignature.compareTo(sSignature) == 0)
                 //lsMatch.add("Mask: " + sMask + " Signature: " + sSignature + " Word: " + sWord);
-                lsMatch.add(sWord);
+                //lsMatch.add(sMask + "|" + sWord);
+                lasMatrix.add(calcMatrix(sWord));
         }
 
         private String calcSignature(String sWord) {
@@ -64,15 +70,35 @@ class MrBrute {
             return sResult.toString();
         }
 
-        List<String> getMatches(){
-            return lsMatch;
+        private String[] calcMatrix(String sWord) {
+            List<String> lsResult = new ArrayList<>();
+            for (int j = 0; j < sWord.length(); j++) {
+                String Para = sWord.substring(j, j + 1) + sMask.substring(j, j + 1);
+                if (!lsResult.contains(Para))
+                    lsResult.add(Para);
+            }
+            Set<String> sQ = new LinkedHashSet<>(lsResult);
+            return sQ.toArray(new String[0]);
         }
 
+        String[][] getMatrixArray() {
+            return lasMatrix.toArray(new String[0][0]);
+        }
 
+        int getMatrixCount() {
+            return lasMatrix.size();
+        }
 
-        int getMatchCount(){
+        String getSignature() {
+            return sSignature;
+        }
+/*
+        int getMatchCount() {
             return lsMatch.size();
         }
-
+        List<String> getMatches() {
+            return lsMatch;
+        }
+*/
     }
 }
