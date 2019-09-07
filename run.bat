ddff0eb76b93655e78cb41cca8588f2da26b7f8d
AlexKot97 <alex.g97@yandex.ru>
2019-09-07 11:25:39 +0300

    Stable search for dictionary. Version 1.0

--- run.bat
+++ run.bat
@@ -1,2 +1,5 @@
-xcopy out\artifacts\KDecryptorNG_jar\KDecryptorNG.jar
+echo off
+xcopy out\artifacts\KDecryptorNG_jar\KDecryptorNG.jar /Y
+echo "Loading...";
+echo on
 java -jar out\artifacts\KDecryptorNG_jar\KDecryptorNG.jar
